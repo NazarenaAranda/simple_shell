e17b677f2fc97c4d0e9710b40c6c936a069dd902
Nazarena Aranda <98347450+NazarenaAranda@users.noreply.github.com>
2022-04-24 13:18:35 -0300

    strcpy

--- free.c
+++ free.c
@@ -4,6 +4,7 @@
  * _free - funcion que libera dos punteros
  * @grid: doble puntero para ser liberado
  * @height: altura de la array que se va pasar
+ * Return: void
  */
 
 void _free(char **grid, int height)
--- strcpy.c
+++ strcpy.c
@@ -1,6 +1,17 @@
 #include "main.h"
 
 /**
- * _strcpy - 
- * 
+ * _strcpy - copa una string
+ * @dest: str copiada
+ * @src: str a copiar
+ * Return: string copiada
  */
+
+char *_strcpy(char *dest, char *src)
+{
+    int a;
+
+    for (a = 0; a >= _strlen(src); a++)
+        dest[a] = src[a];
+        return (dest);
+}
